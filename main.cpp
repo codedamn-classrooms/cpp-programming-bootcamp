e04f5b35d1b44b4d73236bb6d76996b04d8ba7b4
Pranav <pranav.techiegeek@gmail.com>
2023-02-19 23:44:49 +0530

    new code

--- main.cpp
+++ main.cpp
@@ -6,84 +6,124 @@
 
 using namespace std;
 
+/*
+Previously I talked about how you can loop with for. We can also continue looping as long as a condition is true with a while loop. While loops are used when you don't know how many times you will have to loop.
+
+ Here we’ll generate a random number with the random module and randrange(). We will then use the while loop to guess the random value and output it.
+*/
+
 int main()
 {
 
-    // Another thing every programming language must do is to provide a way to
-    // perform the same action multiple times. The keyword for is one one way in
-    // which you can loop through code while executing repeatedly.
+    // Generate a random number
+    srand(time(NULL));
+    // Generate a random number up to 10
+    int randNum = rand() % 100;
 
-    // For as long as you have more hamburger to eat, keep eating that hamburger.
-    // For loops come in many forms.
-
-    // Cycle through each value and print it
-    for (int i = 0; i <= 10; ++i)
+    // The value we increment in the while loop is defined before the loop
+    int i = 1;
+    while (i != randNum)
     {
-        cout << i << endl;
+        i += 1;
     }
+    cout << "The Random Number is " << i << endl;
 
-    // You can cycle through arrays which are lists of values
-    int arr1[] = {1, 2, 3};
-    // We have to calculate the array size by providing a sample
-    // of the type of data it takes up
-    int arrSize = sizeof(arr1) / sizeof(arr1[0]);
-    for (int i = 0; i < arrSize; ++i)
+    /*
+    Break & Continue
+
+    Break and continue are very useful. Continue stops executing the code that remains in the loop and jumps back to the top. While break ends execution and jumps directly to the code that lies immediately outside of the loop.
+
+    Here we’ll cycle from 0 to 20 with a while loop. If a number is even will use continue to skip printing it. If it is odd we’ll print it. We’ll then end execution with break if the value ever reaches 15.
+    */
+
+    int j = 1;
+
+    while (j <= 20)
     {
-        cout << arr1[i] << endl;
-    }
-
-    // An abbreviated for loop
-    for (auto x : arr1)
-        cout << x << endl;
-
-    // PROBLEM
-    // Now before you solve a problem I want to teach you how to test if a number
-    // is odd or even. If you divide any even number by 2 it will not have a
-    // remainder. The modulus operator provides the remainder of a division as
-    // I covered previously.
-
-    // So if num % 2 == 0 we know that num is an even value.
-    int num = 4;
-    string isEven = ((num % 2) == 0) ? "Even" : "Odd";
-    cout << "Even or Odd : " << isEven << "\n";
-
-    // Use the knowledge you have gained to print out all odd numbers from 1 to 20.
-    // Feel free to look at everything on this page to help
-
-    // SOLUTION
-    for (int i = 0; i <= 20; ++i)
-    {
-        if ((i % 2) != 0)
+        // If a value is even don't print it
+        if ((j % 2) == 0)
         {
-            cout << i << endl;
+            j += 1;
+
+            // Continue skips the rest of the code
+            // and jumps back to the beginning
+            // of the loop
+            continue;
         }
+
+        // Break stops execution of the loop and jumps
+        // to the line after the loops closing }
+        if (j == 15)
+            break;
+
+        cout << j << endl;
+
+        // Increment i so the loop eventually ends
+        j += 1;
     }
 
     // PROBLEM
-    // In this problem you will calculate how much money a person will have after
-    // investing for 10 years. Compounding interest is the act of reinvesting each years
-    // interest payment and then receiving interest on the initial value as well as on
-    // interest payments.
+    /*
+    For this problem I want you to draw a pine tree after asking the user for the number of rows. This problem is the most difficult you have had so far, but it will teach a lot. Feel free to use any resources online to solve it.
 
-    // Your program will :
-    // 1. Have the user enter their investment amount and expected interest (Initial investment + additional added at end of each year)
-    // 2. Each year their investment will increase by their investment + their investment
-    // * the interest rate
-    // 3. Print out their earnings after a 10 year period
+    Here is the sample program
 
-    float investment, interest, total;
+    How tall is the tree : 5
+        #
+       ###
+      #####
+     #######
+    #########
+        #
 
-    cout << "How Much to Invest : ";
-    cin >> investment;
-    total = investment;
-    cout << "Interest Rate : ";
-    cin >> interest;
-    interest = interest * .01;
-    for (int i = 0; i < 10; ++i)
+    Here are some additional tips to help you solve the problem.
+
+    Tip 1 : You should use a while loop and 3 for loops.
+
+    Tip 2 : I know that this is the number of spaces and hashes for the tree
+    4 - 1
+    3 - 3
+    2 - 5
+    1 - 7
+    0 - 9
+    Spaces before stump = Spaces before top
+
+    TIP 3 : You will need to do the following in your program
+    1. Decrement spaces by one each time through the loop
+    2. Increment the hashes by 2 each time through the loop
+    3. Save spaces to the stump by calculating tree height - 1
+    4. Decrement from tree height until it equals 0
+    5. Print spaces and then hashes for each row
+    6. Print stump spaces and then 1 hash
+    */
+
+    int treeHeight;
+    cout << "How Tall is the Tree : ";
+    cin >> treeHeight;
+    int spaces = treeHeight - 1;
+    int hashes = 1;
+    int stumpSpaces = treeHeight - 1;
+
+    while (treeHeight != 0)
     {
-        total = total + investment + (total * interest);
+        for (int k = 0; k < spaces; ++k)
+        {
+            cout << " ";
+        }
+        for (int l = 0; l < hashes; ++l)
+        {
+            cout << "#";
+        }
+        cout << endl;
+        spaces -= 1;
+        hashes += 2;
+        treeHeight -= 1;
     }
-    printf("Investment After 10 Years : %.2f\n", total);
+    for (int m = 0; m < stumpSpaces; ++m)
+    {
+        cout << " ";
+    }
+    cout << "#\n";
 
     return 0;
 }
