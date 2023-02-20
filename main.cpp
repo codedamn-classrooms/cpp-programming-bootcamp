41596fb006b55fc1d24f71f42f7c0e6de24534dc
Pranav <pranav.techiegeek@gmail.com>
2023-02-20 09:21:32 +0530

    new file

--- main.cpp
+++ main.cpp
@@ -6,125 +6,89 @@
 #include <array>
 #include <vector>
 #include <sstream>
-#include <algorithm> // Needed for find
-
+#include <algorithm>
 using namespace std;
+
+// ----- FUNCTION PROTOTYPES -----
+// You define the return type and data types for functions
+// using a function prototype so main knows what is to be
+// expected from this function
+
+double AddNumbers(double num1, double num2);
+
+void AssignAge(int age);
+
+int AssignAge2(int age);
 
 int main()
 {
 
-    // Use vectors when you don't know how big your
-    // arrays may be
+    // ----- FUNCTIONS -----
+    // Code that gets above 8 to 10 lines should be broken off
+    // into a separate function to better organize your code
+    // and to promote reuse
 
-    // Create a vector with 2 spaces to start
-    vector<int> vecRandNums(2);
+    // ----- FUNCTION 1 -----
 
-    // Add values
-    vecRandNums[0] = 10;
-    vecRandNums[1] = 20;
+    double num1, num2;
 
-    // Add another value to the vector
-    vecRandNums.push_back(30);
+    cout << "Enter Num 1 : ";
+    cin >> num1;
 
-    // Get size of vector
-    cout << "Vector Size : " << vecRandNums.size() << endl;
+    cout << "Enter Num 2 : ";
+    cin >> num2;
 
-    // Get last value
-    cout << "Last Index : " << vecRandNums[vecRandNums.size() - 1] << endl;
+    // Call the function
+    printf("%.1f + %.1f = %.1f\n", num1, num2, AddNumbers(num1, num2));
 
-    // Check if value in vector
-    // Create an iterator that is used to cycle through
-    // a range of values
-    vector<int>::iterator it;
-    // If find finds 20 it provides an iterator
-    it = find(vecRandNums.begin(), vecRandNums.end(), 20);
-    // We can get the value using the dereference operator
-    // which I'll cover in the next video
-    cout << *it << endl;
+    // ----- END OF FUNCTION 1 -----
 
-    // Convert a string into an array
-    string sSentence = "This is a random string";
+    // ----- FUNCTION 2 -----
 
-    // Create a vector
-    vector<string> vecsWords;
+    // Variables in a function are local to that function
 
-    // A stringstream object receives strings separated
-    // by a space and then spits them out 1 by 1
-    stringstream ss(sSentence);
+    // Variables values changed in a function don't
+    // translate globally
+    int age = 43;
+    AssignAge(age);
+    cout << "New Age " << age << endl;
 
-    // Will temporarily hold each word in the string
-    string sIndivStr;
+    // ----- END OF FUNCTION 2 -----
 
-    // Defines what separates the words
-    char cSpace = ' ';
+    // ----- FUNCTION 3 -----
 
-    // While there are more words to extract keep
-    // executing
-    // getline takes strings from a stream of words stored
-    // in the stream and each time it finds a blanks space
-    // it stores the word proceeding the space in sIndivStr
-    while (getline(ss, sIndivStr, cSpace))
-    {
+    // You could change the value by passing it back
+    age = AssignAge2(age);
+    cout << "New Age " << age << endl;
 
-        // Put the string into a vector
-        vecsWords.push_back(sIndivStr);
-    }
-
-    // Cycle through each index in the vector and print
-    // out each word
-    for (int i = 0; i < vecsWords.size(); ++i)
-    {
-        cout << vecsWords[i] << endl;
-    }
-
-    // PROBLEM
-    // Enter calculation (ex. 5 + 6) : 10 - 6
-    // 10.0 - 6.0 = 4.0
-
-    double dbNum1 = 0, dbNum2 = 0;
-    string sCalc = "";
-    vector<string> vecsCalc;
-
-    cout << "Enter calculation (ex. 5 + 6): ";
-    getline(cin, sCalc);
-
-    stringstream ss2(sCalc);
-    string indivStr;
-    char space = ' ';
-
-    while (getline(ss2, indivStr, space))
-    {
-        vecsCalc.push_back(indivStr);
-    }
-
-    dbNum1 = stoi(vecsCalc[0]);
-    dbNum2 = stoi(vecsCalc[2]);
-    string operation = vecsCalc[1];
-
-    if (operation == "+")
-    {
-        printf("%.1f + %.1f = %.1f\n", dbNum1, dbNum2,
-               (dbNum1 + dbNum2));
-    }
-    else if (operation == "-")
-    {
-        printf("%.1f - %.1f = %.1f\n", dbNum1, dbNum2,
-               (dbNum1 - dbNum2));
-    }
-    else if (operation == "*")
-    {
-        printf("%.1f * %.1f = %.1f\n", dbNum1, dbNum2,
-               (dbNum1 * dbNum2));
-    }
-    else if (operation == "/")
-    {
-        printf("%.1f / %.1f = %.1f\n", dbNum1, dbNum2,
-               (dbNum1 / dbNum2));
-    }
-    else
-    {
-        cout << "Please enter only +, -, *, or /\n";
-    }
+    // ----- END OF FUNCTION 3 -----
 
     return 0;
+}
+
+// FUNCTIONS
+
+// ----- FUNCTIONS -----
+// A function starts with the data type it will return or void
+// if no value is returned
+// You also list the data types for the parameters passed into
+// a function and you may assign them default values
+
+// FUNCTION 1
+double AddNumbers(double num1 = 0, double num2 = 0)
+{
+    return num1 + num2;
+}
+
+// FUNCTION 2
+void AssignAge(int age)
+{
+    age = 24;
+}
+
+// FUNCTION 3
+int AssignAge2(int age)
+{
+    age = 24;
+    return age;
 }
